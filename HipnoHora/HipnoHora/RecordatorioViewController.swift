ad8e80a2afc39cbf96305dbe895e3348321e0437
Contreras Rocha Josue Mosiah <msi_98_98@yahoo.com.mx>
2019-09-23 20:00:19 -0500

    HipnoHora - Uso de notificaciones

---
+++ HipnoHora/HipnoHora/RecordatorioViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RecordatorioViewController.swift
+//  HipnoHora
+//
+//  Created by Contreras Rocha Josue Mosiah on 9/23/19.
+//  Copyright © 2019 Contreras Rocha Josue Mosiah. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class RecordatorioViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Inicializar notificación
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (autorizado, error) in
+            print("¿El usuario autorizó? \(autorizado) ")
+        })
+    }
+    
+    @IBAction func agendarCita() {
+        let horaLocal = self.datePicker.date.addingTimeInterval(TimeInterval(TimeZone.current.secondsFromGMT()))
+        
+        // Notificaciones del usuario
+        let notificacion = UNMutableNotificationContent()
+        notificacion.title = "Hipnotizamiento"
+        notificacion.body = "Llegó la hora de tu sesión"
+        notificacion.badge = 1
+        notificacion.sound = UNNotificationSound.default
+        let disparo = UNCalendarNotificationTrigger.init(dateMatching: Calendar.current.dateComponents([.day, .month, .hour, .minute], from: datePicker.date), repeats: true)
+        let request = UNNotificationRequest(identifier: "Hipnosis", content: notificacion, trigger: disparo)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        print(request.trigger!)
+    }
+}
