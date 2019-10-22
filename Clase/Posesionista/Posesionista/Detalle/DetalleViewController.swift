562040695efd25de246d55e9133b841fb45afc30
Contreras Rocha Josue Mosiah <msi_98_98@yahoo.com.mx>
2019-10-21 19:54:54 -0500

    Uso de UIImagePickerController en Posesionista
    Elementos:
      - Uso de camara
      - Pedir permisos para usar camara y galeria
      - Añadido de ToolBar

--- Clase/Posesionista/Posesionista/Detalle/DetalleViewController.swift
+++ Clase/Posesionista/Posesionista/Detalle/DetalleViewController.swift
@@ -8,24 +8,80 @@
 
 import UIKit
 
-class DetalleViewController: UIViewController {
+class DetalleViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var campoNombre: UITextField!
     @IBOutlet weak var campoSerie: UITextField!
     @IBOutlet weak var campoPrecio: UITextField!
     @IBOutlet weak var labelFecha: UILabel!
+    @IBOutlet weak var foto: UIImageView!
     
-    var cosaADetallar: Cosa!
+    var cosaADetallar: Cosa! {
+        didSet {
+            navigationItem.title = self.cosaADetallar.nombre
+        }
+    }
+    
+    let formatoDeFecha: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    let formatoDePrecio: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.campoNombre.text = self.cosaADetallar.nombre
-        self.campoSerie.text = self.cosaADetallar.numeroDeSerie
-        self.campoPrecio.text = "$\(self.cosaADetallar.valorEnPesos)"
-        self.labelFecha.text = "\(self.cosaADetallar.fechaDeCreacion)"
+        
+        // Delegado
+        self.campoNombre.delegate = self
+        self.campoPrecio.delegate = self
+        self.campoSerie.delegate = self
+        
+        // Mostrar texto
+        self.campoNombre.text = cosaADetallar.nombre
+        self.campoSerie.text = cosaADetallar.numeroDeSerie
+        self.campoPrecio.text = formatoDePrecio.string(from: NSNumber(value: cosaADetallar.valorEnPesos))
+        self.labelFecha.text = formatoDeFecha.string(from: cosaADetallar.fechaDeCreacion)
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.cosaADetallar.nombre = self.campoNombre.text ?? ""
+        self.cosaADetallar.numeroDeSerie = self.campoSerie.text ?? ""
+        if let valor = self.campoPrecio.text, let valorEntero = self.formatoDePrecio.number(from: valor) {
+            self.cosaADetallar.valorEnPesos = valorEntero.intValue
+        } else {
+            self.cosaADetallar.valorEnPesos = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func tomaFoto(_ sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        
+        // Configurar tipo de cámera
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        // Delegado
+        picker.delegate = self
+        
+        // Presentar
+        present(picker, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- Clase/Posesionista/Posesionista/Tabla/CosasTableViewController.swift
+++ Clase/Posesionista/Posesionista/Tabla/CosasTableViewController.swift
@@ -22,13 +22,16 @@ class CosasTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tableView.reloadData()
     }
 
     // MARK: - Table view data source
