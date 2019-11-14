4e1a57291578688d609839fcefc8284c879f00c9
Contreras Rocha Josue Mosiah <msi_98_98@yahoo.com.mx>
2019-11-13 19:45:44 -0600

    Size Classes - Uso en diferentes dispositivos

---
+++ Clase/SizeClasses/SizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Contreras Rocha Josue Mosiah on 11/13/19.
+//  Copyright © 2019 Contreras Rocha Josue Mosiah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
